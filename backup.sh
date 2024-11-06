c5cdd0d295787f8d7eaf94f577df2eb7683e131c
Savalone <116199241+Savalone@users.noreply.github.com>
2024-11-06 17:21:38 -0500

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+volumenes=$(sudo docker volume ls -q | grep single-node)
+directorio="/tmp/wazuh-backup"
+mkdir "$directorio"
+sudo chmod 777 "$directorio"
+
+for volumen in $volumenes; do
+    echo "Haciendo copia de seguridad del volumen $volumen"
+    sudo docker run --rm -v "$volumen":/data -v "$directorio":/backup busybox tar cvf /backup/"$volumen".tar /data &>/dev/null
+done
+
+sudo chown gtech:gtech "$directorio"/*
+mv "$directorio" $(pwd)
